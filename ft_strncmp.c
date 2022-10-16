bf91cb3d1a5ceb45591bcaafb5aa1cc1d20a75e0
Oussama <90373269+Glossophobic@users.noreply.github.com>
2022-10-16 16:55:19 +0100

    ...

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -34,3 +34,28 @@ int main()
 	printf("dyalna  : %d\n", ft_strncmp(str1, str2, 0));
 	printf("dyalhom : %d", strncmp(str1, str2, 0));
 }*/
+/*
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*z1;
+	unsigned char	*z2;
+
+	z1 = (unsigned char*)s1;
+	z2 = (unsigned char*)s2;
+	i = 0;
+	if (n == 0)
+		return (0);
+	if (*z1 == '\0')
+		return (-(*z2));
+	if (*z2 == '\0')
+		return (*z1);
+	while (z1[i] != '\0' || z2[i] != '\0')
+	{
+		if (i == n - 1 || z1[i] != z2[i])
+			return (z1[i] - z2[i]);
+		i++;
+	}
+	return (z1[i] - z2[i]);
+}
+*/
